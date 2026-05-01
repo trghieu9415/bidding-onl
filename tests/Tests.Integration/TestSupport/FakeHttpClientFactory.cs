3dc6ed4ecd434edc80283a07914c823b882be6c9
trghieu9415 <the.first9415@gmail.com>
2026-05-01 20:45:17 +0700

    xóa toàn bộ unit test trong test tích hợp

--- tests/Tests.Integration/TestSupport/FakeHttpClientFactory.cs
+++
@@ -1,7 +0,0 @@
-namespace Tests.Integration.TestSupport;
-
-internal sealed class FakeHttpClientFactory(HttpClient client) : IHttpClientFactory {
-  public HttpClient CreateClient(string name) {
-    return client;
-  }
-}
